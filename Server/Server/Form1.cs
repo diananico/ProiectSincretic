2e75dd87658541841893b2fac78ea8b0a8468fde
rg2796 <rg2796@yahoo.com>
2018-05-22 20:47:57 +0300

    Server stuff

---
+++ Server/Server/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TCPLibrary;
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text = "13000";
+            comboBox1.SelectedIndex = 0;
+        }
+        int option;
+       
+
+        Thread main_thread = null;
+        private void client_handler(TcpClient client)
+        {
+            byte[] buffer_send = new byte[4];
+            byte[] buffer_receive = new byte[4];
+            int i;
+            
+            while ((i = TCP_API.receive(client, ref buffer_receive)) != 0)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("Received: {0} {1} {2} {3}", buffer_receive[0], buffer_receive[1], buffer_receive[2], buffer_receive[3]);
+                
+                //LOGIC GOES HERE
+                TCP_VAR var1 = new TCP_VAR(buffer_receive);
+                buffer_send = var1.Calculate_Results(option);
+                
+                /*
+                for (int j = 0; j < i; j++)
+                    buffer_send[j] = (byte)(buffer_receive[j] + 1);
+               */
+
+                TCP_API.send(client, buffer_send);
+
+            }
+            Console.WriteLine(i);
+            client.Close();
+        }
+
+        public void server_thread(String ip,int port)
+        {
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse(ip);
+                server = new TcpListener(localAddr, port);
+                server.Start();
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection... ");
+                    
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    new Thread(delegate () { client_handler(client); }).Start();
+
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+               server.Stop();
+            }
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs earg)
+        {
+            string IP = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+            textBox1.Text = IP;
+            int port = Int32.Parse(textBox2.Text);
+            main_thread = new Thread(delegate () { server_thread(IP, port); });
+            main_thread.IsBackground = true;
+            main_thread.Start();
+            button1.Enabled = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            option = comboBox1.SelectedIndex+1;
+            Console.WriteLine(option);
+        }
+    }
+}
---
+++ Server/Server/TCP_VAR.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class TCP_VAR
+    {
+        private const int I_MAX = 16;
+        private const int O_MAX = 16;
+        private bool[] I = new bool[I_MAX];
+        private bool[] O = new bool[O_MAX];
+
+        public bool get_bit(byte b,int position)
+        {
+            if (((b >> position) & 1) == 1)
+                return true;
+            else
+                return false;
+        }
+
+        public byte set_bit(byte b, int positon)
+        {
+            return b|= (byte)(1 << positon);
+        }
+        public TCP_VAR(byte[] buff)
+        {
+            int j = 0;
+            for (int i = 0; i < buff.Length && j < I_MAX; i++)
+            {
+                I[j++] = get_bit(buff[i], 7);
+                I[j++] = get_bit(buff[i], 6);
+                I[j++] = get_bit(buff[i], 5);
+                I[j++] = get_bit(buff[i], 4);
+                I[j++] = get_bit(buff[i], 3);
+                I[j++] = get_bit(buff[i], 2);
+                I[j++] = get_bit(buff[i], 1);
+                I[j++] = get_bit(buff[i], 0);
+               
+            }
+            for(int i=0;i<I_MAX;i++)
+             System.Console.WriteLine("I["+i+"]= "+I[i]);
+        }
+
+        public byte[] Calculate_Results(int option)
+        {
+            O[0]= O[1] = O[2] = O[3] = O[4] = false;
+            if(I[0])
+            {
+                return Convert_resullt();
+            }
+            if (I[5])
+            {
+                O[3] = I[3];
+                O[4] = I[4];
+                return Convert_resullt();
+            }
+            if (option == 1)
+            {
+                if (I[1] && I[2])
+                {
+                    return Convert_resullt();
+                }
+                else
+                {
+                    O[3] = I[3];
+                    O[4] = I[4];
+                    if (I[6] && I[4])
+                    {
+                        O[1] = I[1];
+                        O[2] = I[2];
+                    }
+                    if (I[8] && I[3])
+                    {
+                        O[1] = I[1];
+                        O[2] = I[2];
+                    }
+                    if (I[7] && I[4] && I[2])
+                    {
+                        O[2] = I[2];
+                    }
+                    if (I[7] && I[3] && I[1])
+                    {
+                        O[1] = I[1];
+                    }
+                }
+
+            }
+            else
+                if (option == 2)
+            {
+                if ((I[6] && I[7]) || (I[6] && I[8]) || (I[7] && I[8]) || (I[6] &&I[7] && I[8]))
+                {
+                    O[0] = true;
+                }
+                else
+                {
+                    O[3] = I[3];
+                    O[4] = I[4];
+                    if (I[7])
+                    {
+                        if(I[3])
+                        {
+                            O[1] = I[1];
+                        }
+                        if (I[4])
+                        {
+                            O[2] = I[2];
+                        }
+                    }
+                    else
+                    {
+                        if (I[1] && I[2])
+                        {
+                            return Convert_resullt();
+                        }
+                        else
+                        {
+                            
+                            if (I[6] && I[4])
+                            {
+                                O[1] = I[1];
+                                O[2] = I[2];
+                            }
+                            if (I[8] && I[3])
+                            {
+                                O[1] = I[1];
+                                O[2] = I[2];
+                            }
+                           
+                        }
+
+                    }
+                }
+
+            }
+
+            return Convert_resullt();
+        }
+
+        public byte[] Convert_resullt()
+        {
+
+            byte[] buff=new byte[O_MAX/8];
+            int j = 0;
+            for (int i=0;i<O.Length;i++)
+            {
+                System.Console.WriteLine("O[" + i + "]= " + O[i]);
+                j = i / 8;
+                if(O[i])
+                {
+
+                    
+                    buff[j] = set_bit(buff[j], 7-(i % 8));
+                }
+                
+            }
+
+            System.Console.WriteLine(buff[0]);
+            return buff;
+        }
+    }
+}
